d3e4824b09afbee1f75723e749781f481e9fa136
ronacs3 <quanghoangminh2001@gmail.com>
2023-08-21 17:26:03 +0700

    update signin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import Layout from '../../components/Layout';
-import { AirVentIcon, Droplets, Thermometer } from 'lucide-react';
+import { AirVentIcon, AreaChart, BarChart3, Droplets, Thermometer } from 'lucide-react';
 import React from 'react';
 import { Switch } from 'antd';
 import Link from 'next/link';
 export default function Home() {
     return (
         <Layout>
-            <div className="border h-screen bg-slate-50">
+            <div className="border h-screen">
                 <div className="border px-10 py-8 flex flex-row">
                     <div className="w-4/5 ">
                         <div className="text-2xl font-medium">BOARD 1</div>
@@ -28,27 +28,18 @@ export default function Home() {
                         </div>
                     </div>
                 </div>
-                <div className="h-5/6 pt-8 ">
-                    <div className="h-3/5 px-20">
-                        Chart
-                        <div className="h-full w-full border">1</div>
-                    </div>
-                    <div className="h-2/5 pt-10">
-                        <div className="px-20">Device</div>
-                        <div className="grid grid-cols-5 gap-20 px-20 pt-8">
-                            <div className="border rounded-3xl px-6 py-5">
-                                <div className="flex gap-20 pb-14">
-                                    <div className="pt-1">
-                                        <AirVentIcon />
-                                    </div>
-                                    <div>
-                                        <Switch size="small" defaultChecked />
-                                    </div>
-                                </div>
-                                <div>Air Condition</div>
-                                <div>16°C</div>
-                            </div>
+                <div className="px-10 pt-8 h-5/6 ">
+                    {/* Chart */}
+                    <div className="h-4/6 pb-3">
+                        <div className="flex flex-row pb-4">
+                            <BarChart3 className="pt-1" />
+                            <div className=" text-lg">Chart</div>
                         </div>
+                        <div className="border rounded-lg h-5/6 bg-amber-300"></div>
+                    </div>
+                    {/* Device */}
+                    <div className="h-2/6 ">
+                        <div>Device</div>
                     </div>
                 </div>
             </div>
